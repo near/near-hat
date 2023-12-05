eb9889b084e901e39435a3cfd2fda6921fee5712
Morgan Mccauley <morgan@mccauley.co.nz>
2023-12-05 14:35:37 +1300

    refactor: Correctly configure `explorer_indexer` image

--- near-hat/src/containers/explorer_indexer.rs
+++ near-hat/src/containers/explorer_indexer.rs
@@ -1,89 +1,37 @@
-use crate::validator::ValidatorContainer;
 use crate::DockerClient;
 use testcontainers::core::WaitFor;
 use testcontainers::{Container, GenericImage, RunnableImage};
 
 pub struct ExplorerIndexer<'a> {
     pub container: Container<'a, GenericImage>,
-    pub bucket_name: String,
-    pub region: String,
-    pub rpc_address: String,
 }
 
 impl<'a> ExplorerIndexer<'a> {
-    pub const CONTAINER_RPC_PORT: u16 = 3030;
-
     pub async fn run(
         docker_client: &'a DockerClient,
         network: &str,
-        s3_address: &str,
-        bucket_name: String,
-        region: String,
     ) -> anyhow::Result<ExplorerIndexer<'a>> {
-        tracing::info!(
-            network,
-            s3_address,
-            bucket_name,
-            region,
-            "starting NEAR Lake Indexer container"
-        );
+        tracing::info!(network, "starting NEAR Lake Indexer container");
 
         let image = GenericImage::new(
-            "ghcr.io/near/near-lake-indexer",
-            "e6519c922435f3d18b5f2ddac5d1ec171ef4dd6b",
+            "us-central1-docker.pkg.dev/pagoda-data-stack-dev/cloud-run-source-deploy/indexer-explorer",
+            "439ccdab3dfa60f503bf1ddcc4498595d5ad6339",
         )
         .with_env_var("AWS_ACCESS_KEY_ID", "FAKE_LOCALSTACK_KEY_ID")
         .with_env_var("AWS_SECRET_ACCESS_KEY", "FAKE_LOCALSTACK_ACCESS_KEY")
-        .with_wait_for(WaitFor::message_on_stderr("Starting Streamer"))
-        .with_exposed_port(Self::CONTAINER_RPC_PORT);
+        .with_env_var("DATABASE_URL", "test")
+        .with_wait_for(WaitFor::message_on_stderr("Starting Streamer"));
+
         let image: RunnableImage<GenericImage> = (
             image,
-            vec![
-                "--endpoint".to_string(),
-                s3_address.to_string(),
-                "--bucket".to_string(),
-                bucket_name.clone(),
-                "--region".to_string(),
-                region.clone(),
-                "--stream-while-syncing".to_string(),
-                "sync-from-latest".to_string(),
-            ],
+            vec!["mainnet".to_string(), "from-genesis".to_string()],
         )
             .into();
         let image = image.with_network(network);
         let container = docker_client.cli.run(image);
-        let ip_address = docker_client
-            .get_network_ip_address(&container, network)
-            .await?;
-        let rpc_address = format!("http://{}:{}", ip_address, Self::CONTAINER_RPC_PORT);
 
-        tracing::info!(
-            bucket_name,
-            region,
-            rpc_address,
-            "NEAR Lake Indexer container is running"
-        );
-        Ok(ExplorerIndexer {
-            container,
-            bucket_name,
-            region,
-            rpc_address,
-        })
-    }
+        tracing::info!("NEAR Explorer Indexer container is running");
 
-    pub fn host_rpc_address_ipv4(&self) -> String {
-        let host_port = self.container.get_host_port_ipv4(Self::CONTAINER_RPC_PORT);
-        format!("http://127.0.0.1:{host_port}")
-    }
-
-    pub fn host_rpc_address_ipv6(&self) -> String {
-        let host_port = self.container.get_host_port_ipv6(Self::CONTAINER_RPC_PORT);
-        format!("http://[::1]:{host_port}")
-    }
-}
-
-impl<'a> ValidatorContainer<'a> for ExplorerIndexer<'a> {
-    fn validator_container(&self) -> &Container<'a, GenericImage> {
-        &self.container
+        Ok(ExplorerIndexer { container })
     }
 }
