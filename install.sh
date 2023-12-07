7b22de5944f27cf2f75c0d41d939503a6d8f3fa2
pkudinov <pkudinov@gmail.com>
2023-12-06 19:43:26 -0800

    Add missing command

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ mkdir -pv $(brew --prefix)/etc/
 echo 'address=/.nearhat/127.0.0.1' >> $(brew --prefix)/etc/dnsmasq.conf
 sudo mkdir -v /etc/resolver
 sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/nearhat'
+sudo brew services start dnsmasq
