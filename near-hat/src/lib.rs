76307abe63b417fdb73930345c78ae678a85cb64
Morgan McCauley <morgan@mccauley.co.nz>
2023-12-08 08:28:51 +1300

    feat: Add explorer frontend (#9)
    * feat: Add explorer frontend
    
    * refactor: Use dockerhub images

--- near-hat/src/lib.rs
+++ near-hat/src/lib.rs
@@ -5,18 +5,18 @@ mod validator;
 
 pub use client::DockerClient;
 
-use ctx::explorer_indexer::ExplorerIndexerCtx;
+use ctx::explorer::ExplorerCtx;
 use ctx::lake_indexer::LakeIndexerCtx;
 use ctx::nearcore::NearcoreCtx;
-use ctx::relayer::RelayerCtx;
 use ctx::queryapi::QueryApiCtx;
+use ctx::relayer::RelayerCtx;
 
 pub struct NearHat<'a> {
     pub queryapi_ctx: QueryApiCtx<'a>,
     pub lake_indexer_ctx: LakeIndexerCtx<'a>,
     pub nearcore_ctx: NearcoreCtx,
     pub relayer_ctx: RelayerCtx<'a>,
-    pub explorer_indexer_ctx: ExplorerIndexerCtx<'a>,
+    pub explorer_ctx: ExplorerCtx<'a>,
 }
 
 impl<'a> NearHat<'a> {
@@ -27,23 +27,25 @@ impl<'a> NearHat<'a> {
         let lake_indexer_ctx = LakeIndexerCtx::new(docker_client, network).await?;
         let nearcore_ctx = NearcoreCtx::new(&lake_indexer_ctx.worker).await?;
         let relayer_ctx = RelayerCtx::new(docker_client, network, &nearcore_ctx).await?;
-        let explorer_indexer_ctx =
-            ExplorerIndexerCtx::new(docker_client, network, &lake_indexer_ctx).await?;
         let queryapi_ctx = QueryApiCtx::new(
-            docker_client, network, 
-            &relayer_ctx.redis.redis_address, 
-            &lake_indexer_ctx.localstack.s3_address, 
-            &lake_indexer_ctx.localstack.s3_bucket, 
+            docker_client,
+            network,
+            &relayer_ctx.redis.redis_address,
+            &lake_indexer_ctx.localstack.s3_address,
+            &lake_indexer_ctx.localstack.s3_bucket,
             &lake_indexer_ctx.localstack.s3_region,
             &nearcore_ctx,
-            &lake_indexer_ctx.lake_indexer.rpc_address).await?;
+            &lake_indexer_ctx.lake_indexer.rpc_address,
+        )
+        .await?;
+        let explorer_ctx = ExplorerCtx::new(docker_client, network, &lake_indexer_ctx).await?;
 
         Ok(NearHat {
             queryapi_ctx,
             lake_indexer_ctx,
             nearcore_ctx,
             relayer_ctx,
-            explorer_indexer_ctx,
+            explorer_ctx,
         })
     }
 }
