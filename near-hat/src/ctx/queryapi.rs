2fe6c46121eef6432f794f443055d4736cda6c9a
Darun Seethammagari <darun@near.org>
2023-12-08 10:06:20 -0800

    Ignore files and update hasura graphql to auto deploy config (#11)
    * Ignore files and update hasura graphql to auto deploy config
    
    * Use absolute paths for hasura docker volume

--- near-hat/src/ctx/queryapi.rs
+++ near-hat/src/ctx/queryapi.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use near_token::NearToken;
 
 use crate::client::DockerClient;
@@ -6,10 +8,6 @@ use crate::containers::hasura_auth::HasuraAuth;
 use crate::containers::queryapi_postgres::QueryApiPostgres;
 use crate::containers::hasura_graphql::HasuraGraphql;
 use crate::containers::runner::Runner;
-use std::fs::{File, self};
-use std::io::{Write, self};
-use std::process::Command;
-use std::path::Path;
 
 use super::nearcore::NearcoreCtx;
 
@@ -42,9 +40,6 @@ impl<'a> QueryApiCtx<'a> {
         let hasura_auth = HasuraAuth::run(docker_client, network).await?;
         let postgres = QueryApiPostgres::run(docker_client, network).await?;
         let hasura_graphql = HasuraGraphql::run(docker_client, network, &hasura_auth.auth_address, &postgres.connection_string).await?;
-        if let Err(e) = Self::update_config_and_deploy_hasura(&hasura_graphql.host_address_ipv4(), Path::new("./hasura")) {
-            eprintln!("Failed to update config and run 'hasura deploy': {}", e);
-        }
         let coordinator = Coordinator::run(
             docker_client, 
             network, 
@@ -81,35 +76,5 @@ impl<'a> QueryApiCtx<'a> {
             coordinator,
             runner
         })
-    }
-
-    fn update_config_and_deploy_hasura(endpoint: &str, hasura_folder: &Path) -> io::Result<()> {
-        // Step 1: Update config.yaml
-        let config_content = format!(
-            "version: 3\nendpoint: {}\nadmin_secret: myadminsecretkey\nmetadata_directory: metadata\nactions:\n  kind: synchronous\n  handler_webhook_baseurl: http://localhost:3000",
-            endpoint
-        );
-        
-        let config_path = hasura_folder.join("config.yaml");
-        let mut file = File::create(config_path)?;
-        file.write_all(config_content.as_bytes())?;
-    
-        // Step 2: Run hasura deploy
-        let original_dir = std::env::current_dir()?;
-        std::env::set_current_dir(hasura_folder)?;
-    
-        let output = Command::new("hasura")
-            .arg("deploy")
-            .output()?;
-    
-        if output.status.success() {
-            println!("Hasura deploy executed successfully.");
-        } else {
-            eprintln!("Error in running Hasura deploy: {}", String::from_utf8_lossy(&output.stderr));
-        }
-        // Step 3: Revert current directory
-        std::env::set_current_dir(original_dir)?;
-    
-        Ok(())
     }
 }
